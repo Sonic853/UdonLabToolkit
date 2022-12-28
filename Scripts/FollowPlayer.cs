b3dbf980d9ba4156af52261f86df4c5855037a0a
Sonic853 <Sonic853@qq.com>
2022-12-28 14:24:12 +0800

    Add FollowPlayer and UdonOnceEnter

---
+++ Scripts/FollowPlayer.cs
@@ -0,0 +1,41 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace UdonLab.Toolkit
+{
+    public class FollowPlayer : UdonSharpBehaviour
+    {
+        private VRCPlayerApi _player;
+        /// <summary>
+        /// 最长距离，超过这个距离将拉至这个距离，-1 为不限制
+        /// </summary>
+        [Header("最长距离，超过这个距离将拉至这个距离")]
+        [SerializeField] private float _maxDistance = 1;
+        void LateUpdate()
+        {
+            if (_player == null)
+            {
+                _player = Networking.LocalPlayer;
+                return;
+            }
+            var playerPosition = _player.GetPosition();
+            if (_maxDistance != -1)
+            {
+                // 限制 transform.position 到 _player.GetPosition() 的距离，超过 _maxDistance 将拉至 _maxDistance
+                // 计算 transform.position 到 _player.GetPosition() 的距离
+                float distance = Vector3.Distance(transform.position, playerPosition);
+                if (distance > _maxDistance)
+                {
+                    // 将 transform.position 拉至 _player.GetPosition() 的 _maxDistance 距离
+                    transform.position = Vector3.MoveTowards(transform.position, playerPosition, distance - _maxDistance);
+                }
+            }
+            transform.position = Vector3.Lerp(transform.position, playerPosition, Time.deltaTime * 10);
+            // 只改变 Y 轴 Lerp
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, _player.GetRotation().eulerAngles.y, 0), Time.deltaTime * 10);
+        }
+    }
+}
---
+++ Scripts/UdonOnceEnter.cs
@@ -0,0 +1,23 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace UdonLab.Toolkit
+{
+    public class UdonOnceEnter : UdonSharpBehaviour
+    {
+        [Header("UdonSharpBehaviour")]
+        [SerializeField] private UdonSharpBehaviour behaviour;
+        [SerializeField] private bool isLocalOnly = true;
+        public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+        {
+            if (isLocalOnly && !player.isLocal)
+                return;
+            behaviour.SetProgramVariable("_OnPlayerTriggerEnter_VRCPlayerApi", player);
+            behaviour.SendCustomEvent("_OnPlayerTriggerEnter");
+            gameObject.SetActive(false);
+        }
+    }
+}
